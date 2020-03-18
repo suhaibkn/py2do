c4f844d0122411cc23b8efb5775b6ffbc76f5dd4
Suhaib Khan <suhaib96@gmail.com>
2020-03-18 12:29:23 +0530

    Dynamically push new entry in resp object

--- static/js/app.js
+++ static/js/app.js
@@ -1,12 +1,22 @@
 var app = new Vue({
-    el: '.main',
+    el: '#app',
     data() {
         return {
-            todos: null,
+            todos: [],
             loading: false,
+        }
+    },
+    methods: {
+        newTodo: function (event) {
+            event.preventDefault()
+            let val = {
+                todo: document.getElementById('new-todo').value,
+            }
+            this.todos.push(val)
+            document.getElementById('new-todo').value = ''
         }
     },
     mounted() {
         axios.get('http://127.0.0.1:5000/api/get/active').then(response => (this.todos = response.data))
-    }
+    },
 });
